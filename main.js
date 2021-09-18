95f27e1bf15d3be34c0ad839e0b59b8b562c0199
Animoh-co <81622182+Animoh-co@users.noreply.github.com>
2021-09-18 15:57:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/SAiNF0DTT/models.json", modelload);
+
+}
+
+function modelload(){
+    classifier.classify(gotresult);
+
+}
+
+function gotresult(error, result){
+    if(error)
+    console.error(error);
+    else 
+    console.log(result);
+}
